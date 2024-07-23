4e17d785f08b783891d312f377cb1dcc9c4abe4d
gabryelreyes <gabryelreyes@users.noreply.github.com>
2024-07-23 14:09:37 +0000

    deploy: 222e8bed049e187fcf8079bec991f270e6e42f76

---
+++ search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "012abcdeghilmprsv~",
+  1: "bdeilmps",
+  2: "r",
+  3: "abcdegilmprs~",
+  4: "d",
+  5: "h"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "variables",
+  5: "groups"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Variables",
+  5: "Modules"
+};
+
